323b9b3213aedb0fb67e3ca4c722cf0f2808ee19
Giorgio Vidali <gvidali@stuy.edu>
2016-11-29 17:09:02 -0500

    Working Book

--- 01Library/Book.java
+++ 01Library/Book.java
@@ -1,2 +1,44 @@
 public class Book{
+private String author;
+private String title;
+private String isbn;
+
+public Book(){
+
+};
+public Book(String newAuthor, String newTitle, String newISBN){
+  author = newAuthor;
+  title = newTitle;
+  isbn = newISBN;
+}
+private boolean SetAuthor(String newAuthor){
+author = newAuthor;
+return true;
+}
+private boolean SetTitle(String newTitle){
+title = newTitle;
+return true;
+}
+private boolean SetISBN(String newISBN){
+isbn = newISBN;
+return true;
+}
+private String getAuthor(){
+
+return author;
+}
+private String getTitle(){
+return title;
+}
+private String getISBN(){
+return isbn;
+}
+
+public String toString(){
+  return author + ", " + title + ", " + isbn;
+}
+
+public static void main(String[] args) {
+
+}
 }
