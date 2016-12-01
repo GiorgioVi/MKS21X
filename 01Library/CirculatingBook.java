f2b1e89ca534b5e29d944d4f56ef129687c4ead6
Giorgio Vidali <gvidali@stuy.edu>
2016-11-30 21:01:28 -0500

    this is working

--- 01Library/CirculatingBook.java
+++ 01Library/CirculatingBook.java
@@ -1,3 +1,66 @@
 public class CirculatingBook extends LibraryBook{
-  
+
+private String currentHolder;
+private String dueDate;
+
+
+public CirculatingBook(String newAuthor, String newTitle, String newISBN,
+                   String newcallNumber){
+super(newAuthor, newTitle, newISBN, newcallNumber);
+}
+
+
+public boolean setAuthor(String newAuthor){
+super.setAuthor(newAuthor);
+return true;
+}
+public boolean setTitle(String newTitle){
+super.setTitle(newTitle);
+return true;
+}
+public boolean setISBN(String newISBN){
+super.setISBN(newISBN);
+return true;
+}
+public boolean setcallNumber(String newCall){
+super.setcallNumber(newCall);
+return true;
+}
+public String getAuthor(){
+return super.getAuthor();
+}
+public String getTitle(){
+return super.getTitle();
+}
+public String getISBN(){
+return super.getISBN();
+}
+public String getcallNumber(){
+return super.getcallNumber();
+}
+
+public void checkout(String patron, String dueDatex){
+  currentHolder = patron;
+  dueDate = dueDatex;
+}
+public void returned(){
+  currentHolder = null;
+  dueDate = null;
+}
+public String circulationStaus(){
+  if(currentHolder == null){
+    return getTitle() + " is available on shelves";
+  }
+  else{
+    return currentHolder + ", " + dueDate;
+  }
+}
+public String toString(){
+  if (currentHolder == null){
+    return super.toString();
+  }
+  else{
+    return super.toString() + ", " + circulationStaus();
+  }
+}
 }
--- 01Library/ReferenceBook.java
+++ 01Library/ReferenceBook.java
@@ -1,3 +1,66 @@
 public class ReferenceBook extends LibraryBook{
-  
+
+private String collection;
+
+
+  public ReferenceBook(String newAut, String newTit,
+   String newISBN, String callNumer0, String newcollection){
+     super(newAut, newTit, newISBN, callNumer0);
+     collection = newcollection;
+
+  }
+  public boolean setAuthor(String newAuthor){
+  super.setAuthor(newAuthor);
+  return true;
+  }
+  public boolean setTitle(String newTitle){
+  super.setTitle(newTitle);
+  return true;
+  }
+  public boolean setISBN(String newISBN){
+  super.setISBN(newISBN);
+  return true;
+  }
+  public boolean setcallNumber(String newCall){
+  super.setcallNumber(newCall);
+  return true;
+  }
+  public boolean setCollection(String newcollect){
+  collection = newcollect;
+  return true;
+  }
+  public String getAuthor(){
+  return super.getAuthor();
+  }
+  public String getTitle(){
+  return super.getTitle();
+  }
+  public String getISBN(){
+  return super.getISBN();
+  }
+  public String getcallNumber(){
+  return super.getcallNumber();
+  }
+  public String getCollection(){
+  return collection;
+  }
+
+
+  public void checkout(String patron, String duedate){
+     System.out.println(patron + " cannot check out a ReferenceBook");
+
+  }
+  public void returned(){
+    System.out.println("reference book could not have been checked out -- return impossible");
+  }
+  public String circulationStaus(){
+    return "non-circulating reference book";
+
+  }
+  public String toString(){
+    return super.toString() + ", " + getCollection();
+
+  }
+
+
 }
