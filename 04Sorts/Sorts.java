aa855b7d4321e0b14d366dd92aaca424c23fc668
Giorgio Vidali <gvidali@stuy.edu>
2016-12-13 05:54:13 -0500

    later

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,30 @@
+public class Sorts{
+
+public static String name(){
+	return "09.Vidali.Giorgio";
+}
+
+public static void selectionSort(int[] data){
+	
+	int mininum = data[0];
+	for(int i = 0; i < data.length() - 1; i++){
+		for(int c = 0; c < data.length();c++){
+			if(data[i] < ){
+			minimum = data[i];		 		
+		}	
+}
+}
+}
+
+public static void main(String [] argghhhhh){
+	int[] tester = {7,4,5,8,2,3};
+	selectionSort(tester);
+	System.out.println(tester[0]);
+	System.out.println(tester[1]);
+	System.out.println(tester[2]);
+	System.out.println(tester[3]);
+	System.out.println(tester[4]);
+	System.out.println(tester[5]);
+}
+
+}
